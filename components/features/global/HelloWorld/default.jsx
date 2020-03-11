a51fc2efa9b657e7274f1dd6192d6835915b646e
Todd Fleeman <Todd.Fleeman@coxinc.com>
2020-03-11 18:39:44 -0400

    trying a few things to refresh my memory

--- components/features/global/HelloWorld/default.jsx
+++ components/features/global/HelloWorld/default.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const HelloWorld = () => {
-  const text = 'Hello Worldly people';
-  return <div class="boop">{text}</div>;
+  const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse purus eros, consectetur quis est nec, pulvinar faucibus justo. Donec posuere ultricies ornare. Donec lobortis ex id bibendum efficitur. Quisque sit amet sem eget odio pharetra vulputate sed eu dolor. Nam cursus ante tellus, ut fermentum lacus rutrum varius. Morbi tempus metus arcu, vitae tempor dui pellentesque ut. Maecenas nec purus suscipit augue tincidunt consequat in ut lacus. Fusce tortor dolor, cursus quis tempor a, gravida a nisl. In aliquet metus non vulputate pellentesque. Praesent cursus enim ac interdum porta.';
+  return <div>{text}</div>;
 };
 
 export default HelloWorld;
