e00259222936c2a2206a61431b63d5520f1644cb
Dotun Ogunsakin <email@dotun.me>
2020-06-26 18:52:56 -0400

    [CSDEVFEG-973] Copied over work from api-author-play branch.

---
+++ components/features/global/Leadership/default.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useContent } from 'fusion:content';
+
+const Leadership = (props) => {
+  const { slugs } = props.customFields;
+
+  const allSlugs = slugs.split(', ');
+  const leaders = [];
+
+  allSlugs.forEach((slug) => {
+    const content = useContent({
+      source: 'author-api',
+      query: { slug },
+    });
+
+    leaders.push(content);
+  });
+
+  const itemsArray = leaders.length > 0 && leaders.map((item, index) => {
+    // console.log('item: ', item.authors[0]);
+
+    return (
+      <div key={index} className="col-md-4">
+        <a href={item.authors[0].bio_page}>
+          <div className="image-holder">
+            <img src={item.authors[0].image} alt={item.authors[0].byline} />
+          </div>
+          <div className="content-holder">
+            <h4 className="headline">{item.authors[0].byline}</h4>
+            <summary>{item.authors[0].role}</summary>
+          </div>
+        </a>
+      </div>
+    );
+  });
+
+  return (
+    <div className="leadership">
+      <div className='row'>
+        { itemsArray }
+      </div>
+    </div>
+  );
+};
+
+Leadership.label = 'Leadership';
+
+Leadership.propTypes = {
+  customFields: PropTypes.shape({
+    slugs: PropTypes.string.tag({
+      group: 'Feature options',
+      label: 'Slugs',
+    }),
+  }),
+};
+
+export default Leadership;
