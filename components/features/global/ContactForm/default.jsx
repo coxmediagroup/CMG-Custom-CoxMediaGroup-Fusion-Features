e6926b34fda80e95bf1e5e6d9a0f8f75a81b778f
Dotun Ogunsakin <email@dotun.me>
2020-07-15 13:33:16 -0400

    [CSDEVFEG-970] Added environment-specific contact processing values; lint fixes.

--- components/features/global/ContactForm/default.jsx
+++ components/features/global/ContactForm/default.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import * as EmailValidator from 'email-validator';
+import { CONTACT_ENDPOINT_URL, CONTACT_ENDPOINT_API_KEY } from 'fusion:environment';
 
 class ContactForm extends React.Component {
   constructor(props) {
@@ -12,17 +13,17 @@ class ContactForm extends React.Component {
       contact_subject: '',
       contact_message: '',
       submission_status: '',
-      submission_messages: []
+      submission_messages: [],
     };
   }
 
   handleChange = (e) => {
-    const {name, value} = e.target;
-    this.setState({[name]: value});
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   }
 
   /**
-   * 
+   *
    * @param {Array} list
    * @param {Array} classNames
    */
@@ -37,15 +38,15 @@ class ContactForm extends React.Component {
   }
 
   /**
-   * 
+   *
    * @param {object} dataObj required fields {name: string, email: string, message: string}
    * @returns {object} {valid: boolean, messages: []}
    */
   validateUserData = (dataObj) => {
     const result = {
       valid: true,
-      messages: []
-    }
+      messages: [],
+    };
 
     // name
     if (!dataObj.name.trim()) {
@@ -75,36 +76,36 @@ class ContactForm extends React.Component {
       name: this.state.contact_name.trim(),
       email: this.state.contact_email.trim(),
       subject: this.state.contact_subject.trim(),
-      message: this.state.contact_message.trim()
+      message: this.state.contact_message.trim(),
     };
 
     const userDataValidation = this.validateUserData(userData);
 
-    if(!userDataValidation.valid) {
+    if (!userDataValidation.valid) {
       this.setState({
         submission_status: 'ERROR',
-        submission_messages: userDataValidation.messages
+        submission_messages: userDataValidation.messages,
       });
 
       return;
     }
 
     // Send email
-    const url = 'https://39soq7gn4a.execute-api.us-east-1.amazonaws.com/dev/api/v1/coxreps/contact'; // TODO: Update with DST values (can we have envs in Arc?)
     const params = {
       method: 'POST',
       headers: {
-        'x-api-key': 'r4PanNhSal40lfoedC02D10oPxztz58ga4hXBZpz' // TODO: Update with DST values (can we have envs in Arc?)
+        'x-api-key': CONTACT_ENDPOINT_API_KEY,
       },
-      body: JSON.stringify(userData)
+      body: JSON.stringify(userData),
     };
-    fetch(url, params)
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-      submission_status: data.status,
-      submission_messages: [data.message]
-    })});
+    fetch(CONTACT_ENDPOINT_URL, params)
+      .then((response) => { response.json(); })
+      .then((data) => {
+        this.setState({
+          submission_status: data.status,
+          submission_messages: [data.message],
+        });
+      });
   }
 
   render() {
@@ -114,41 +115,62 @@ class ContactForm extends React.Component {
           <div className="col-md-12 contact-form">
             <h3>HAVE QUESTIONS?</h3>
             <h5>LET US KNOW HOW WE CAN HELP!</h5>
-  
-            {this.state.submission_status === "SUCCESS" &&
-            <div className="contact-form-message">
-              <ul className="list-unstyled">
-                {this.printList(this.state.submission_messages)}
-              </ul>
-            </div>}
-  
-            {this.state.submission_status === "ERROR" &&
-            (<div className="error-message-box">
-              <p>Validation errors occurred. Please confirm the fields and submit it again.</p>
-              <ul className="list-unstyled">
-                {this.printList(this.state.submission_messages, ['error-message'])}
-              </ul>
-            </div>)}
-  
+
+            {this.state.submission_status === 'SUCCESS'
+            && (<div className="contact-form-message">
+                  <ul className="list-unstyled">
+                    {this.printList(this.state.submission_messages)}
+                  </ul>
+                </div>)}
+
+            {this.state.submission_status === 'ERROR'
+            && (<div className="error-message-box">
+                  <p>Validation errors occurred. Please confirm the fields and submit it again.</p>
+                  <ul className="list-unstyled">
+                    {this.printList(this.state.submission_messages, ['error-message'])}
+                  </ul>
+                </div>)}
+
             <form onSubmit={this.handleSubmit} method="post" className="mailform_form">
               <div className="form-group">
                 <label>Your Name (Required)
-                  <input id="id_contact_name" className="form-control" name="contact_name" value={this.state.contact_name} onChange={this.handleChange} />
+                  <input id="id_contact_name"
+                         className="form-control"
+                         name="contact_name"
+                         value={this.state.contact_name}
+                         onChange={this.handleChange}
+                  />
                 </label>
               </div>
               <div className="form-group">
                 <label>Your Email (Required)
-                  <input id="id_contact_email" className="form-control" name="contact_email" value={this.state.contact_email} onChange={this.handleChange} />
+                  <input id="id_contact_email"
+                         className="form-control"
+                         name="contact_email"
+                         value={this.state.contact_email}
+                         onChange={this.handleChange}
+                  />
                 </label>
               </div>
               <div className="form-group">
                 <label>Subject
-                  <input id="id_subject" className="form-control" name="contact_subject" value={this.state.contact_subject} onChange={this.handleChange} />
+                  <input id="id_subject"
+                         className="form-control"
+                         name="contact_subject"
+                         value={this.state.contact_subject}
+                         onChange={this.handleChange}
+                  />
                 </label>
               </div>
               <div className="form-group">
                 <label>Your Message (Required)
-                  <textarea id="contact_message" className="form-control" rows="3" name="contact_message" value={this.state.contact_message} onChange={this.handleChange}></textarea>
+                  <textarea id="contact_message"
+                            className="form-control"
+                            rows="3"
+                            name="contact_message"
+                            value={this.state.contact_message}
+                            onChange={this.handleChange}
+                  ></textarea>
                 </label>
               </div>
               <p className="submit">
@@ -160,7 +182,7 @@ class ContactForm extends React.Component {
       </div>
     );
   }
-};
+}
 
 ContactForm.label = 'Contact Form';
 
