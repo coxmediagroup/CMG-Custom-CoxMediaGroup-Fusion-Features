3f14666e8a53027c91fcb1ff0c270367a91801d9
Dotun Ogunsakin <email@dotun.me>
2020-07-17 09:44:04 -0400

    [CSDEVFEG-970] Added submission status constants; hide form when submission is successful.

--- components/features/global/ContactForm/default.jsx
+++ components/features/global/ContactForm/default.jsx
@@ -5,6 +5,12 @@ import * as EmailValidator from 'email-validator';
 // TODO switch to CMG-DST values once server-side handler is deployed to CMG-DST
 const CONTACT_ENDPOINT_URL = 'https://39soq7gn4a.execute-api.us-east-1.amazonaws.com/dev/api/v1/coxreps/contact';
 const CONTACT_ENDPOINT_API_KEY = 'r4PanNhSal40lfoedC02D10oPxztz58ga4hXBZpz';
+const submissionStatuses = {
+  ERROR_VALIDATION: 'ERROR:VALIDATION',
+  ERROR_SUBMISSION: 'ERROR:SUBMISSION',
+  SUBMITTING: 'SUBMITTING',
+  SUCCESS: 'SUCCESS',
+};
 
 class ContactForm extends React.Component {
   constructor(props) {
@@ -86,7 +92,7 @@ class ContactForm extends React.Component {
 
     if (!userDataValidation.valid) {
       this.setState({
-        submission_status: 'ERROR:VALIDATION',
+        submission_status: submissionStatuses.ERROR_VALIDATION,
         submission_messages: userDataValidation.messages,
       });
 
@@ -103,22 +109,22 @@ class ContactForm extends React.Component {
     };
 
     this.setState({
-      submission_status: 'SUBMITTING',
+      submission_status: submissionStatuses.SUBMITTING,
       submission_messages: [],
     });
 
     fetch(CONTACT_ENDPOINT_URL, params)
       .then((response) => { return response.json(); })
       .then((data) => {
-        const submissionStatus = data.status.toUpperCase();
+        const responseStatus = data.status.toUpperCase();
 
         this.setState({
-          submission_status: submissionStatus,
+          submission_status: responseStatus,
           submission_messages: [data.message],
         });
 
         // Reset form if submission is successful
-        if (submissionStatus === 'SUCCESS') {
+        if (responseStatus === submissionStatuses.SUCCESS) {
           this.setState({
             contact_name: '',
             contact_email: '',
@@ -129,7 +135,7 @@ class ContactForm extends React.Component {
       })
       .catch(() => {
         this.setState({
-          submission_status: 'ERROR:SUBMISSION',
+          submission_status: submissionStatuses.ERROR_SUBMISSION,
           submission_messages: ['Error while attempting to send feedback. Please try again later.'],
         });
       });
@@ -143,14 +149,14 @@ class ContactForm extends React.Component {
             <h3>HAVE QUESTIONS?</h3>
             <h5>LET US KNOW HOW WE CAN HELP!</h5>
 
-            {this.state.submission_status.toUpperCase() === 'SUCCESS'
+            {this.state.submission_status.toUpperCase() === submissionStatuses.SUCCESS
             && (<div className="contact-form-message">
                   <ul className="list-unstyled">
                     {this.printList(this.state.submission_messages)}
                   </ul>
                 </div>)}
 
-            {this.state.submission_status.toUpperCase() === 'ERROR:VALIDATION'
+            {this.state.submission_status.toUpperCase() === submissionStatuses.ERROR_VALIDATION
             && (<div className="error-message-box">
                   <p>Validation errors occurred. Please confirm the fields and submit it again.</p>
                   <ul className="list-unstyled">
@@ -158,61 +164,62 @@ class ContactForm extends React.Component {
                   </ul>
                 </div>)}
 
-            {this.state.submission_status.toUpperCase() === 'ERROR:SUBMISSION'
+            {this.state.submission_status.toUpperCase() === submissionStatuses.ERROR_SUBMISSION
             && (<div className="error-message-box">
                   <ul className="list-unstyled">
                     {this.printList(this.state.submission_messages, ['error-message'])}
                   </ul>
                 </div>)}
 
-            <form onSubmit={this.handleSubmit} method="post" className="mailform_form">
-              <div className="form-group">
-                <label>Your Name (Required)
-                  <input id="id_contact_name"
-                         className="form-control"
-                         name="contact_name"
-                         value={this.state.contact_name}
-                         onChange={this.handleChange}
-                  />
-                </label>
-              </div>
-              <div className="form-group">
-                <label>Your Email (Required)
-                  <input id="id_contact_email"
-                         className="form-control"
-                         name="contact_email"
-                         value={this.state.contact_email}
-                         onChange={this.handleChange}
-                  />
-                </label>
-              </div>
-              <div className="form-group">
-                <label>Subject
-                  <input id="id_subject"
-                         className="form-control"
-                         name="contact_subject"
-                         value={this.state.contact_subject}
-                         onChange={this.handleChange}
-                  />
-                </label>
-              </div>
-              <div className="form-group">
-                <label>Your Message (Required)
-                  <textarea id="contact_message"
-                            className="form-control"
-                            rows="3"
-                            name="contact_message"
-                            value={this.state.contact_message}
-                            onChange={this.handleChange}
-                  ></textarea>
-                </label>
-              </div>
-              <p className="submit">
-                <button type="submit" onSubmit={this.handleSubmit}>
-                  {this.state.submission_status.toUpperCase() === 'SUBMITTING' ? 'SUBMITTING...' : 'SUBMIT'}
-                </button>
-              </p>
-            </form>
+            {this.state.submission_status.toUpperCase() !== submissionStatuses.SUCCESS
+            && (<form onSubmit={this.handleSubmit} method="post" className="mailform_form">
+                <div className="form-group">
+                  <label>Your Name (Required)
+                    <input id="id_contact_name"
+                          className="form-control"
+                          name="contact_name"
+                          value={this.state.contact_name}
+                          onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label>Your Email (Required)
+                    <input id="id_contact_email"
+                          className="form-control"
+                          name="contact_email"
+                          value={this.state.contact_email}
+                          onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label>Subject
+                    <input id="id_subject"
+                          className="form-control"
+                          name="contact_subject"
+                          value={this.state.contact_subject}
+                          onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label>Your Message (Required)
+                    <textarea id="contact_message"
+                              className="form-control"
+                              rows="3"
+                              name="contact_message"
+                              value={this.state.contact_message}
+                              onChange={this.handleChange}
+                    ></textarea>
+                  </label>
+                </div>
+                <p className="submit">
+                  <button type="submit" onSubmit={this.handleSubmit}>
+                    {this.state.submission_status.toUpperCase() === submissionStatuses.SUBMITTING ? 'SUBMITTING...' : 'SUBMIT'}
+                  </button>
+                </p>
+              </form>)}
           </div>
         </div>
       </div>
